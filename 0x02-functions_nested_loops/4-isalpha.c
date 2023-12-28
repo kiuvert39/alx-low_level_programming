3284b123a0b951eb98a3a33bfb3b88fb32aadecb
kiuvert39 <galvinegbe@gmail.com>
2023-12-28 03:01:43 -0800

    all functions

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _isalpha - check id a letter is an alphbet or not
- * @c: character to be verified
- *
- * Retrun: 1 or 0
+ * _isalpha - tests whether a character is from the English alphabet.
+ * @c: character to be checked.
+ * Return: 1 if the character is an English character.
+ * 0 if the character is not an English character.
  */
 int _isalpha(int c)
 {
